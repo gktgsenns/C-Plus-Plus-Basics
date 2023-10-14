ab177cad7515a0193dbfd254abe544c5bb682716
gktgsenns <139327946+gktgsenns@users.noreply.github.com>
2023-10-14 14:15:25 +0300

    Add files via upload

---
+++ login.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string username = "user";
+    std::string password = "password";
+    std::string inputUsername;
+    std::string inputPassword;
+
+    std::cout << "Username: ";
+    std::cin >> inputUsername;
+    std::cout << "Password: ";
+    std::cin >> inputPassword;
+
+    if (inputUsername == username && inputPassword == password) {
+        std::cout << "Login successful!" << std::endl;
+    } else {
+        std::cout << "Login failed. Incorrect username or password." << std::endl;
+    }
+
+    return 0;
+}
